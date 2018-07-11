dedfd94a9625dc6db7aa6d30a6d17f48d43735f8
59naga <i59naga@icloud.com>
2018-07-12 08:00:29 +0900

    :white_check_mark: #2の再現テストを追加

--- test/index.mjs
+++ test/index.mjs
@@ -3,6 +3,10 @@ import assert from 'assert'
 import bosses from '../dist/gbf-raid-bosses.json'
 import indexes from '../dist/indexes.json'
 
-it('和名からbossesの該当オブジェクトの参照を取得できるべき', () => {
+it('和名からbossesの該当オブジェクトの参照を取得できるべき(#1)', () => {
   assert(bosses[indexes['Lv60 青竜']])
+})
+
+it('HARD+を取得できるべき(#2)', () => {
+  assert(bosses[indexes['Lv60 ティアマト']])
 })
